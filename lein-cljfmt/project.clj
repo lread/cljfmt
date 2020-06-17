c2bfa3cdc03e416a6bf15a714cae086ff5ab13e4
lread <lee@dlread.com>
2020-06-17 15:22:00 -0400

    Take a crack at upgrading lein-cljfmt to rewrite-cljc
    Point lein-cljfmt at our test version of cljfmt that use rewrite-cljc

--- lein-cljfmt/project.clj
+++ lein-cljfmt/project.clj
@@ -5,4 +5,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[cljfmt "0.6.7"]])
+  :dependencies [[cljfmt
+                  ;; I think specifying the branch name instead of the SHA should work
+                  "lread-rewrite-cljc-test"]]
+  :plugins [[reifyhealth/lein-git-down "0.3.6"]]
+  :middleware [lein-git-down.plugin/inject-properties]
+  :repositories [["public-github" {:url "git://github.com"}]]
+  :git-down {cljfmt {:coordinates lread/cljfmt
+                     :manifest-root "cljfmt"}})
